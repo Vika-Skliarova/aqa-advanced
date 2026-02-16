6288cf831151a45aabec1d7b04c0a3c2870c7576
Viktoriia Kureninova <kureninovavika@gmail.com>
2026-02-16 23:00:13 +0200

    added 'task-1' with variable types

---
+++ js-basics/task-1.js
@@ -0,0 +1,12 @@
+let value_text = 'JavaScript primitive data types';
+let value_number = 12345;
+let value_boolean = true;
+let value_null = null;
+let value_undefined;
+
+
+console.log(value_text);
+console.log(value_number);
+console.log(value_boolean);
+console.log(value_null);
+console.log(value_undefined);
