43e1eeeda14652f770e05b3fd5c62ea104912a39
Viktoriia Kureninova <kureninovavika@gmail.com>
2026-02-22 17:23:25 +0100

    Added 'task-1' Types of functions

---
+++ js-functions/task-1.js
@@ -0,0 +1,18 @@
+// Function declaration
+function calculateArea(width, height) {
+    let area = width * height;
+    return area;
+}
+
+// Function expression
+const areaRectangle = function(width, height) {
+    return width * height;
+} 
+
+// Arrow function expression
+const areaRectangle2 = (width, height) => width * height;
+
+
+console.log(calculateArea(5, 10));
+console.log(areaRectangle(7, 12));
+console.log(areaRectangle2(4, 8));
