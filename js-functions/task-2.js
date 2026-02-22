bfd33191407a82bd9fc5042d44200ceeb84c2873
Viktoriia Kureninova <kureninovavika@gmail.com>
2026-02-22 22:44:07 +0100

    Added changes

--- js-functions/task-2.js
+++ js-functions/task-2.js
@@ -10,9 +10,14 @@ function checkAge2(age) {
     return age >= 18;
 };
 
+const isAdult = (age) => age >= 18;
 
 console.log(checkAge(25));
 console.log(checkAge(15));
 
 console.log(checkAge2(10));
 console.log(checkAge2(21));
+
+
+console.log(isAdult(17));
+console.log(isAdult(19));
