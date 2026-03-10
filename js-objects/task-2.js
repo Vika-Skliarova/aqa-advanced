e7cdb7d947ecd93fa11bb4862a88fd3964358241
Viktoriia Kureninova <kureninovavika@gmail.com>
2026-03-10 15:37:06 +0100

    Added 'task-2' Destruction of objects

---
+++ js-objects/task-2.js
@@ -0,0 +1,11 @@
+const book = {
+    title: 'Title of book',
+    author: 'Author of book',
+    year: 2025
+};
+
+const { title, author, year } = book;
+
+console.log(title);
+console.log(author);
+console.log(year);
