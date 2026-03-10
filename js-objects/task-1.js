8e5a789758a30d115a7779746580c5170242aba5
Viktoriia Kureninova <kureninovavika@gmail.com>
2026-03-10 14:56:49 +0100

    Added 'task-1' - object creation

---
+++ js-objects/task-1.js
@@ -0,0 +1,8 @@
+const student = {};
+
+student.firstName = 'Name';
+student.lastName = 'Lastname';
+student.age = 20;
+student.courses = ['Mathematics', 'History', 'Programming'];
+
+console.log(student);
